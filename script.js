fdd0e3c22fe17e366ace4d55a70febd7acce3c94
Karan Singh <karans7318@gmail.com>
2023-06-08 02:40:04 -0400

    Game function added to choose winner-loser result

--- script.js
+++ script.js
@@ -3,9 +3,13 @@ const options = document.querySelectorAll('.option');
 const rock = document.querySelector('.rock');
 const paper = document.querySelector('.paper');
 const scissors = document.querySelector('.scissors');
-const playerScore = document.getElementById('player--score');
-const computerScore = document.getElementById('computer--score');
 
+let playerScoreEl = document.getElementById('player--score');
+let computerScoreEl = document.getElementById('computer--score');
+
+const newGameBtn = document.getElementById('new-game');
+
+const resultStatement = document.getElementById('result-statement');
 
 // pseudo-code
 // make buttons respond with values - done
@@ -17,26 +21,20 @@ const computerScore = document.getElementById('computer--score');
 // create reset button
 // Go back to CSS and create winner and loser settings (green/red)
 
+let scores, playing;
 // create initial values for reset
-// const init = () =>{
-//     score0El.textContent = 0;
-//     score1El.textContent = 0;
+const init = () =>{
+    playerScoreEl.textContent = 0;
+    computerScoreEl.textContent = 0;
+
+    resultStatement.textContent = '';
     
+    playerScore = 0;
+    computerScore = 0;
 
-//  scores = [0, 0];
-//  currentScore = 0;
-//  activePlayer = 0;
-//  playing = true;
-
-//     current0El.textContent = 0;
-//     current1El.textContent = 0;
-//     diceEl.classList.add('hidden');
-//     player0El.classList.remove('player--winner');
-//     player1El.classList.remove('player--winner');
-//     player0El.classList.add('player--active');
-//     player1El.classList.remove('player--active');
-// }
-// init()
+     playing = true;
+};
+init();
 
 
 const results = ['rock', 'paper', 'scissors'];
@@ -46,41 +44,78 @@ function getComputerChoice() {
 };
 
 options.forEach(option => {
-   option.addEventListener('click', () =>{
 
     function playRound(playerSelection, computerSelection){  
         if (playerSelection === 'rock' && computerSelection === 'scissors') {
             console.log('win')
+            playerScore += 1
+            playerScoreEl.textContent = playerScore
+            resultStatement.textContent = 'You won this round!'
         }
         else if(playerSelection === 'scissors' && computerSelection === 'paper'){
             console.log('win')
+            playerScore += 1
+            playerScoreEl.textContent = playerScore
+            resultStatement.textContent = 'You won this round!'
         }
         else if(playerSelection === 'paper' && computerSelection === 'rock'){
             console.log('win')
+            playerScore += 1
+            playerScoreEl.textContent = playerScore
+            resultStatement.textContent = 'You won this round!'
         }
         // tie
         else if(playerSelection === computerSelection) {
             console.log('tie')
+            resultStatement.textContent = 'You tied!'
         } else {
             console.log('lose')
+            computerScore += 1
+            computerScoreEl.textContent = computerScore
+            resultStatement.textContent = 'You lost this round!'
         }
     };
+
+   option.addEventListener('click', () =>{
+
+    
     const playerSelection = option.value;
     const computerSelection = getComputerChoice();
-   
+    // console.log(playerSelection);
+    // console.log(computerSelection);
+    // playRound(playerSelection, computerSelection); 
 
-    console.log(playerSelection);
-    console.log(computerSelection);
-    playRound(playerSelection, computerSelection); 
-    //    console.log(playRound(playerSelection, computerSelection));
+    if(playing){
+        // game functionality - pick overall winner and reset
+        function game() {
+            if (playerScore === 5){
+                playing = false
+                resultStatement.textContent = 'You won the game! Start a new game.'
+            } 
+            else if (computerScore === 5){
+                playing = false
+                resultStatement.textContent = 'You lost the game! Start a new game.'
+            } 
+            else if (computerScore < 5){
+                playRound(playerSelection, computerSelection); 
+            }
+            else if (playerScore < 5){
+                playRound(playerSelection, computerSelection); 
+            } 
+            else {
+                console.log('you\'re still playing!')
+            }
+          
+            };
+    
+            game();
+    } else{
+        console.log('not playing game')
+    }
+    
    });
 });
 
 
-function game() {
-    playRound(playerSelection, computerSelection); 
-    console.log('pizza')
 
-};
-
-    console.log(game())
+newGameBtn.addEventListener('click', init);
